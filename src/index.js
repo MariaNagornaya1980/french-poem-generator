6714098a94b84dc521def088584bf2b9f08ca060
MariaNagornaya1980 <mariawright205@gmail.com>
2024-08-05 12:50:37 -0700

    added basic Java Script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: [
+      "Vois-tu, je sais que tu m’attends. J’irai par la forêt, j’irai par la montagne",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);

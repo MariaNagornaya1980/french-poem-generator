87a54af2c9e2f877cb6cbcfba33e293ef956fea3
MariaNagornaya1980 <mariawright205@gmail.com>
2024-08-05 13:45:32 -0700

    added typewriter pluggin and AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: [
-      "Vois-tu, je sais que tu m’attends. J’irai par la forêt, j’irai par la montagne",
-    ],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f3tb6c4a39o18b701a82bf50b801c4af";
+  let prompt = `User instructions are: Generate a French poem about ${instructionsInput.value}`;
+  let context = `You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Please do not include the title of the poem and sign with SheCodes AI at the bottom of the poem. Make sure to follow the user instructions.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context:${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
